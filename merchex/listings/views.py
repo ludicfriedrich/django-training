f353bdce7d1cd2833a22728ef306a766d8525a6a
ludicfriedrich <ludicagnihouede@gmail.com>
2023-04-13 10:13:42 +0000

    Contact and articles page

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def hello(request):
     return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def contact(request):
+    return HttpResponse('<h1>Contactez-vous</h1> <p>Page de contact !</p>')
+
+def articles(request):
+    return HttpResponse('<h1>Articles</h1> <p>Nos articles !</p>')
