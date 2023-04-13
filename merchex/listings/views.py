c61d857133e5828fae64a9fceb27f8834020a7e0
ludicfriedrich <ludicagnihouede@gmail.com>
2023-04-13 14:25:49 +0000

    Migrations and get data from database

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,14 +1,38 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from listings.models import Band
+from listings.models import Listing
+
 
 def hello(request):
-    return HttpResponse('<h1>Hello Django!</h1>')
+    bands = Band.objects.all()
+    return HttpResponse(f"""
+        <h1>Hello Django !</h1>
+        <p>Mes groupes préférés sont :<p>
+        <ul> 
+            <li>{bands[0].name}</li>
+            <li>{bands[1].name}</li>
+            <li>{bands[2].name}</li>
+        </ul>
+    """)
+
 
 def about(request):
     return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
 
+
 def contact(request):
     return HttpResponse('<h1>Contactez-vous</h1> <p>Page de contact !</p>')
 
+
 def articles(request):
-    return HttpResponse('<h1>Articles</h1> <p>Nos articles !</p>')
+    listing = Listing.objects.all()
+    return HttpResponse(f"""
+        <h1>Liste</h1>
+        <ul> 
+            <li>{listing[0].title}</li>
+            <li>{listing[1].title}</li>
+            <li>{listing[2].title}</li>
+            <li>{listing[3].title}</li>
+        </ul>
+    """)
