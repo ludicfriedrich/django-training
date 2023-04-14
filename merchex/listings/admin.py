cb6b76b21c1151310e1ae8b0e1b88e9f63d53dcf
ludicfriedrich <ludicagnihouede@gmail.com>
2023-04-14 14:24:53 +0000

    Django admin

--- merchex/listings/admin.py
+++ merchex/listings/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from listings.models import Band
+from listings.models import Listing
+from listings.models import Listing
 
-# Register your models here.
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre') # liste les champs que nous voulons sur l'affichage de la liste
+    
+class ListingAdmin (admin.ModelAdmin):
+    list_display = ('title','year', 'sold')
+
+admin.site.register(Band, BandAdmin) # nous modifions cette ligne, en ajoutant un deuxième argument
+admin.site.register(Listing, ListingAdmin)
--- merchex/listings/models.py
+++ merchex/listings/models.py
@@ -7,14 +7,32 @@ class Band(models.Model):
         SYNTH_POP = 'SP'
         ALTERNATIVE_ROCK = 'AR'
     
-    name = models.fields.CharField(max_length=100)
-    genre = models.fields.CharField(choices=Genre.choices, max_length=5)
-    biography = models.fields.CharField(max_length=1000)
+    name = models.fields.CharField(max_length = 100)
+    genre = models.fields.CharField(choices = Genre.choices, max_length = 5)
+    biography = models.fields.CharField(max_length = 1000)
     year_formed = models.fields.IntegerField(
         validators=[MinValueValidator(1900), MaxValueValidator(2023)]
     )
-    active = models.fields.BooleanField(default=True)
-    official_homepage = models.fields.URLField(null=True, blank=True)
+    active = models.fields.BooleanField(default = True)
+    official_homepage = models.fields.URLField(null=True, blank = True)
+    
+    def __str__(self):
+        return f'{self.name}'
     
 class Listing(models.Model):
-    title = models.fields.CharField(max_length=100)
+    class Type(models.TextChoices):
+        RECORDS = 'Records',
+        CLOTHING = 'Clothing',
+        POSTERS = 'Posters',
+        MISCELLANEOUS = 'Miscellaneous'
+    
+    title = models.fields.CharField(max_length = 100)
+    description = models.fields.CharField(max_length = 500)
+    sold = models.fields.BooleanField(default = False)
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2023)], null = True, blank = True
+    )
+    type = models.fields.CharField(choices = Type.choices, max_length = 25)
+    
+    def __str__(self):
+        return f'{self.title}'
